22464779b4961608eafc573ed408981e6d8edc30
hinayub <hinaayub2002@gmail.com>
2025-08-21 09:43:32 +0500

    second push

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "corsheaders",
     "rest_framework",
-    "product",
+    "products",
 ]
 
 MIDDLEWARE = [
@@ -118,6 +118,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 STATIC_URL = "static/"
 
--- backend/products/models.py
+++ backend/products/models.py
@@ -8,16 +8,17 @@ class Product(models.Model):
     price = models.PositiveIntegerField(default=0)
     stock = models.PositiveIntegerField(default=0)
 
+    def __str__(self):
+        return self.name
+
 
 class ProductColor(models.Model):
     product = models.ForeignKey(
         Product, related_name="colors", on_delete=models.CASCADE
     )
     color_name = models.CharField(max_length=50)
-
-
-class ProductImage(models.Model):
-    product = models.ForeignKey(
-        Product, related_name="images", on_delete=models.CASCADE
-    )
+    hex_code = models.CharField(max_length=7, blank=True)
     image = models.ImageField(upload_to="product_images/")
+
+    def __str__(self):
+        return f"Image for {self.product.name}{self.color_name}"
